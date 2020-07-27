b1ed7c491ada8862aaf33e992dac5c0f7ba186cf
dontpan-ik <64174091+dontpan-ik@users.noreply.github.com>
2020-07-26 20:18:01 -0500

    nuevos cambios falta opciones de filtrado

--- cliente/home.php
+++ cliente/home.php
@@ -8,10 +8,10 @@ include('../modelo/clases.php');
   //$info=$obj->getProductInfo($obj2);
 ?>
 <div class="container-fluid">
-<form action="clientes.php?pagina=1" method="POST">
+<form action="../controlador/productos.php" method="POST">
     <div class="row bg-light text-dark p-2">
         <div class="col-sm-2 col-md-2 col-lg-2 ">
-            <label><img src="../img/title.png" class="img-fluid" alt="Responsive image"></label>             
+            <label><a href="../cliente/home.php"><img src="../img/title.png" class="img-fluid" alt="Responsive image"></a></label>             
         </div>
         <div class="col-sm-8 col-md-8 col-lg-8">
         <div class="form-inline">
@@ -49,7 +49,7 @@ include('../modelo/clases.php');
 
         </div>
         <div class="col-sm-2 col-md-2 col-lg-2">
-            <button type="button" class="btn btn-success mt-2">Buscar Producto</button>
+            <button type="submit" class="btn btn-success mt-2">Buscar Producto</button>
         </div>
     </div>
     <div class="container">
@@ -57,50 +57,43 @@ include('../modelo/clases.php');
     </div>
 </form>
 </div>
-
 <div class="container-fluid">
-    <?php
-
-    /*echo $info[0]->getNombreProd()."<br>";
-    echo $info[0]->getCategoria()."<br>";
-    echo $info[0]->getSubCat()."<br>";
-    echo $info[0]->getPrecio()."<br>";*/
-
-
-    for($i=0;$i<$obj->totalProductos();$i++){
-        $info=$obj->getProductInfo($obj2,$i);
-
-        echo "<form action='carrito.php' method='POST'>";
-			echo "<div class='container'>
-			<div class='row'>
-			<div class='col-xs-12 col-sm-12 col-lg-12 col-xl-12'>
-			<div class='card'>
-			<div class='card-body text-center'>";
-			echo "<table>
-     		<tr>
-        	 <td rowspan='8'><img src='../img/fondo.png' width='100px' height='90px'></td>
-     			</tr>
-     			 <tr>
-         		 <td rowspan='5'><img src='../img/default_img.png' width='190px' height='200px'></td>
-          		<td rowspan='5'><img src='../img/fondo.png' width='100px' height='90px'></td>
-      			</tr>
-     			 <tr>
-         		 <td>".$info->getNombreProd()."</td>
-      			</tr>
-      			<tr>
-      			 <td>".$info->getCategoria()."</td>
-      			 </tr>
-     			 <tr>
-         		 <td>".$info->getSubCat()."</td>
-      			</tr>
-      			<tr>
-         		 <td>".$info->getPrecio()."</td>
-      			</tr>
-                  <tr><td><button type='submit' class='btn btn-warning' name ='id'>Comprar</button></td>
-                  <td><button type='submit' class='btn btn-primary' name ='id'>Agregar al carrito</button></td></tr>
-  				</table>";
-			echo "</div></div></div></div></div><br>";
-			echo "</form>";
-    }
-    ?>
+    <div class="row bg-light text-dark p-2">
+        <div class="col-sm-2 col-md-2 col-lg-2">
+            <form action="../controlador/productos.php" method="POST">
+            <h3>Opciones de Filtrado</h3>
+            </form>
+        </div>
+        <div class="col-sm-10 col-md-10 col-lg-10">
+            <?php for($i=0;$i<$obj->totalProductos();$i++){
+                $info=$obj->getProductInfo($obj2,$i); ?>
+                <form action='carrito.php' method='POST'>
+                    <div class='container'>
+                        <div class='row'>
+                            <div class='col-xs-12 col-sm-12 col-lg-12 col-xl-12'>
+                                <div class='card'>
+                                    <div class='card-body text-center'>
+                                        <table>
+                                            <tr><td rowspan='8'><img src='../img/fondo.png' width='100px' height='90px'></td></tr>
+                                            <tr><td rowspan='5'><img src='../img/default_img.png' width='190px' height='200px'></td>
+                                            <td rowspan='5'><img src='../img/fondo.png' width='100px' height='90px'></td></tr>
+                                            <tr><td><?php echo $info->getNombreProd(); ?></td></tr>
+                                            <tr><td><?php echo $info->getCategoria(); ?></td></tr>
+                                            <tr><td><?php echo $info->getSubCat(); ?></td></tr>
+                                            <tr><td><?php echo $info->getPrecio(); ?></td></tr>
+                                            <tr><td><button type='submit' class='btn btn-warning' name ='id'>Comprar</button></td>
+                                            <td><button type='submit' class='btn btn-primary' name ='id'>Agregar al carrito</button></td>
+                                            <td><button type='submit' class='btn btn-success' name ='id'>Mas Informacion</button></td></tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><br>                
+                </form> 
+            <?php } ?>
+        </div>
+    </div>
 </div>
+
+
